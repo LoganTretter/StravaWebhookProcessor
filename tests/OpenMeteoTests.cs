67751f212915247ce7043db5710dfa8a4cf3b31b
Logan Tretter <Logan.Tretter@gmail.com>
2025-08-10 11:11:04 -0500

    Add a weather test I previously had

---
+++ tests/OpenMeteoTests.cs
@@ -0,0 +1,22 @@
+﻿using OpenMeteoIntegration;
+
+namespace Tests;
+public class OpenMeteoTests
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var client = new OpenMeteoClient();
+
+        var input = new GetWeatherInput
+        {
+            Latitude = decimal.Parse("41.882629"),
+            Longitude = decimal.Parse("-87.623474"),
+            Start = DateTimeOffset.Parse("2024-01-01T12:00:00Z"),
+            End = DateTimeOffset.Parse("2024-01-01T16:00:00Z")
+        };
+
+        var output = await client.GetWeatherData(input);
+        Assert.NotNull(output);
+    }
+}

43c85000821f724d734eb07cb68b1be2ab53c2d8
Logan Tretter <Logan.Tretter@gmail.com>
2025-08-10 20:54:19 -0500

    Use options pattern

---
+++ src/StravaWebhookProcessor/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(StravaWebhookProcessor.Startup))]
+
+namespace StravaWebhookProcessor;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddOptions<StravaWebhookProcessorOptions>()
+            .Configure<IConfiguration>((settings, configuration) =>
+            {
+                configuration.GetSection(nameof(StravaWebhookProcessorOptions)).Bind(settings);
+            });
+    }
+}
---
+++ src/StravaWebhookProcessor/StravaWebhookProcessorOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace StravaWebhookProcessor;
+
+public class StravaWebhookProcessorOptions
+{
+    public string KeyVaultUri { get; set; } = string.Empty;
+    public long StravaAthleteId { get; set; } = long.MinValue;
+    public long StravaWebhookSubscriptionId { get; set; } = long.MinValue;
+    public string StravaWebhookSubscriptionVerificationToken { get; set; } = string.Empty;
+}
